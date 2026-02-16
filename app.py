e03e23bb18da92cac1d9481f0e66aaeb75e30aed
Quoc-Thông NGUYEN <thong1988@icloud.com>
2026-02-16 12:05:44 +0100

    Change default checkbox value for showing password

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if st.button("Generate Password"):
     st.success("✅ Generated password: ")
 
     # Toggle to show/hide password
-    show_password = st.checkbox("Show password", value=True)
+    show_password = st.checkbox("Show password", value=False)
     st.code(password, language="text")
 
     # Separate copy button
